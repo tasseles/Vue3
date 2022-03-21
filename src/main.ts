ef7f711907f0889d9c782409c1af64dfffbb7e10
wangyang1213 <877593729@qq.com>
2022-03-21 11:48:15 +0800

    styles：新增全局样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import {store,key} from './store/index'
+import '@/styles/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //统一导入el-icon图片
--- src/views/goods/goodsCategory/GoodsCategory.vue
+++ src/views/goods/goodsCategory/GoodsCategory.vue
@@ -1,11 +1,7 @@
-<template>
-商品分类
-</template>
+<template>商品分类</template>
 
-<script lang='ts' setup>
-import {ref,reactive} from 'vue'
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
